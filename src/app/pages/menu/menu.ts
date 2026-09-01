1b0c1609f7bfca3ea563e996beebc38c8f3714ee
Lowdy28 <2516360032icastillon@gmail.com>
2026-09-01 14:27:33 -0600

    Connect client menu to the products API using signals

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,12 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-  variants: string[];
-}
+import { ProductService } from '../../services/product';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-menu',
@@ -14,10 +9,14 @@ interface Product {
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {
-  products: Product[] = [
-    { id: 1, name: 'Alitas de sabor', price: 65, variants: ['Mango habanero', 'BBQ', 'Tamarindo chipotle'] },
-    { id: 2, name: 'Hamburguesa arrachera', price: 85, variants: [] },
-    { id: 3, name: 'Sopes', price: 30, variants: ['Pollo verde', 'Tinga', 'Suadero'] },
-  ];
+export class Menu implements OnInit {
+  products = signal<Product[]>([]);
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAll().subscribe((data) => {
+      this.products.set(data);
+    });
+  }
 }
---
+++ src/app/services/product.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:5098/api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+}
